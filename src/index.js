8da8c4f59f114e84f5521cf276c535d0bd8f39c2
chenhebing <1025263994@qq.com>
2019-01-28 14:52:33 +0800

    support exposing resource from dll source

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
+import path from 'path';
+import NormalModule from 'webpack/lib/NormalModule';
+import DelegatedModule from 'webpack/lib/DelegatedModule';
+
 export default class WebpackExposePlugin {
     constructor (options) {
         this.options = options;
     }
 
     apply (compiler) {
-        compiler.hooks.make.tap('expose-require-plugin-compiler', (compilation) => {
+        compiler.hooks.compilation.tap('expose-require-plugin-compiler', (compilation) => {
             compilation.mainTemplate.hooks.beforeStartup.tap('expose-require-plugin-maintemplate', (source) => {
                 const exposeMap = {};
-                compilation.modules.forEach(module => Object.keys(this.options).some(key => {
-                    if (this.options[key] === module.resource) {
-                        exposeMap[key] = module.id;
-                        return true;
+                compilation.modules.forEach(module => {
+                    if (module.userRequest) {
+                        Object.keys(this.options).some(key => {
+                            if (this.options[key] === path.resolve(module.userRequest)) {
+                                if (module instanceof NormalModule) {
+                                    exposeMap[key] = `window.${key} = __webpack_require__('${module.id}');`;
+                                    return true;
+                                }
+                                if (module instanceof DelegatedModule) {
+                                    exposeMap[key] = `window.${key} = __webpack_require__('${module.sourceRequest}')('${module.delegateData.id}');`;
+                                    return true;
+                                }
+                            }
+                            return false;
+                        });
                     }
-                    return false;
-                }));
-                const exposeStr = Object.keys(exposeMap).map(item => `window.${item} = __webpack_require__('${exposeMap[item]}');`).join('\n');
+                });
+                const exposeStr = Object.values(exposeMap).join('\n');
                 return source + exposeStr;
             });
         });
